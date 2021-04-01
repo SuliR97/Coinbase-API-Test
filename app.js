3a07407a996a2fd457842f4b34526e13caed5617
Suli R <sulaymanrashid1@googlemail.com>
2021-04-01 21:08:35 +0100

    Multiple Coins

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
+const { promisify } = require('util');
 const Client = require( "coinbase" ).Client;
 
 const client = new Client({
-	 apiKey: '',
-	 apiSecret: '',
+	 apiKey: '-',
+	 apiSecret: '-',
 	 strictSSL: false
 });
 
-client.getBuyPrice({ currencyPair: 'BTC-USD'}, function(err, obj) {
-	if (err) console.log('error: ' + err);
-	console.log('total amount: ' + obj.data.amount);
-});
+const getBuyPricePromise = promisify(client.getBuyPrice).bind(client);
+
+const currencies = ['BTC-USD', 'ETH-USD'];
+
+async function main() {
+	try {
+		// put your logic inside the try block here:
+
+		const loadPriceRequests = currencies.map(currency => getBuyPricePromise({ currencyPair: currency }));
+		const results = await Promise.all(loadPriceRequests);
+
+		results.forEach((result, i) => {
+			console.log(`Currency: ${currencies[i]}`);
+			console.log(`Total Amount: ${result.data.amount}\n`);
+		});
+
+	} catch (err) {
+		console.log('err: ', err);
+	}
+}
+main();
+
+
